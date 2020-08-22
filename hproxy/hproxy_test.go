304da05276a4a786a108a1f7549d31f252b701b7
maxim-ge <maxim.ge@gmail.com>
2020-08-22 22:31:01 +0300

    misc

--- hproxy/hproxy_test.go
+++ hproxy/hproxy_test.go
@@ -35,31 +35,12 @@ func sgofn() {
 
 func Test_gojit_proxy_panic(t *testing.T) {
 	asm := newAsmGoABI()
-
-	addr := addrOfFunc(gofnpanic)
-	addr2 := reflect.ValueOf(gofnpanic).Pointer()
-
-	fmt.Printf("ADDR= %v, %v, %v \n", addr, addr2, uint64(reflect.ValueOf(gofnpanic).Pointer()))
-	fmt.Printf("ADDR= %x, %x, %x \n", addr, addr2, uint64(reflect.ValueOf(gofnpanic).Pointer()))
-
 	var jitf func()
 	// call gofn
 	{
-		// asm.Sub(amd64.Imm{Val: 0x16}, amd64.Rsp)
-		// asm.MovAbs(uint64(addrOfFunc(hproxy2)), amd64.Rax)
-		// asm.Mov(amd64.Rax, amd64.Indirect{Base: amd64.Rsp, Offset: 0, Bits: 64})
-		// asm.MovAbs(uint64(addrOfFunc(gofn)), amd64.Rax)
-		// asm.JmpRax()
-		// asm.Add(amd64.Imm{Val: 0x16}, amd64.Rsp)
-
-		//asm.MovAbs(uint64(addr), amd64.Rax)
 		asm.MovAbs(uint64(reflect.ValueOf(gofnpanic).Pointer()), amd64.Rax)
 		asm.JmpRax()
-		// asm.CallFuncGo(gofnpanic)
-
-		asm.Ret()
 		asm.BuildTo(&jitf)
-		// buildToInternal(asm.Buf, &jitf, HBuild)
 	}
 
 	jitf()
@@ -84,7 +65,7 @@ func Test_gojit_proxy_call(t *testing.T) {
 		// asm.Mov(amd64.Rax, amd64.Indirect{Base: amd64.Rsp, Offset: 8, Bits: 64})
 
 		asm.MovAbs(uint64(f), amd64.Rax)
-		asm.JmpRax()
+		asm.JmpRax() 
 		asm.Ret()
 		asm.BuildTo(&jitf)
 		// buildToInternal(asm.Buf, &jitf, HBuild)
